fdc1973d3f007a21baee43a73b31a0bd8aff5ac2
[Poonam Chopade] <[poonam.chopade11@gmail.com]>
2022-12-26 17:02:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let img = document.createElement("img");
+let qr = document.querySelector(".qr-code");
+qr.appendChild(img);
+
+
+function generateQR() {
+    let input = document.getElementById("input");
+    if (input.value) {
+        input.style.borderColor = "#c7c7c7";
+        img.src = `https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=180x180&data=${input.value}`;
+    } else {
+        input.style.borderColor = "red"; 
+        return false;
+    }
+    input.value="";
+}
